25839a1fec64428c999d1f43c6acd7d962427469
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-25 20:43:36 +0530

    style menu and cart page

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,20 +1,31 @@
 import { Link } from 'react-router-dom';
 
-function Button({children,disable,to}) {
+function Button({children,disable,to,type}) {
 
-  const className =`bg-yellow-400 uppercase font-semibold
-  text-stone-800 py-3 px-4 tracking-wide rounded-full inline-block
-   transition-all duration-300 hover:bg-yellow-300 focus:outline-none
-   focus:ring focus:ring-yellow-300 focus:ring-offset-2
-   disabled:cursor-not-allowed sm:px-6 sm:py-4`
+ const base =`bg-yellow-400 uppercase font-semibold
+   text-stone-800  tracking-wide rounded-full inline-block
+    transition-all duration-300 hover:bg-yellow-300 focus:outline-none
+    focus:ring focus:ring-yellow-300 focus:ring-offset-2
+    disabled:cursor-not-allowed`;
+   const style= {
+    primary:base + ' py-3 px-4 sm:px-6 sm:py-4',
+    small:base+ ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+
+    secondary:`uppercase font-semibold border-2 border-stone-300
+    text-stone-400  tracking-wide rounded-full inline-block
+     transition-all duration-300 hover:bg-stone-300 hover:text-stone-800
+      focus:outline-none focus:text-stone-800
+     focus:ring focus:ring-stone-200 focus:ring-offset-2
+     disabled:cursor-not-allowed py-2.5 px-4 sm:px-6 sm:py-3.5`
+   }
 
   if(to)
-  return(<Link className={className} to={to}>{children}</Link>)
+  return(<Link className={style[type]} to={to}>{children}</Link>)
   
   return (
     <button
     disabled={disable}
-    className={className}
+    className={style[type]}
     >
     {children}
     </button>
