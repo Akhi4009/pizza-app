c5e219fa7c1774d2fe8ae3c89add43e0e446dd6d
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-01-25 23:40:43 +0530

    style createOrder form

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -40,37 +40,37 @@ function CreateOrder() {
   const isSubmitting = navigation.state === 'submitting'
   const formErrors = useActionData();
   return (
-    <div>
-       <h2>Ready to order? Let&apos;s go!</h2> 
+    <div className='px-4 py-6'>
+       <h2 className='mb-8 text-xl font-semibold'>Ready to order? Let&apos;s go!</h2> 
        <Form method ="POST">
-        <div>
-          <label>First Name</label>
+        <div className='mb-5 flex flex-col sm:flex-row sm:items-center gap-2'>
+          <label className='sm:basis-40'>First Name</label>
           <input
-            className='input' 
+            className='input grow' 
             type="text"
             name="customer"
             required 
           />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
+        <div className='mb-5 flex flex-col sm:flex-row sm:items-center gap-2'>
+          <label className='sm:basis-40'>Phone number</label>
+          <div className="grow">
             <input
-             className='input'
+             className='input w-full'
              type="tel"
              name="phone"
              required
                />
+               {formErrors?.phone && <p className='bg-red-100 text-xs text-red-700 mt-2 p-2 rounded-md'>{formErrors.phone}</p>}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
-        </div>
+         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
+        <div className='mb-5 flex flex-col sm:flex-row sm:items-center gap-2'>
+          <label className='sm:basis-40'>Address</label>
+          <div className='grow'>
             <input 
-              className='input'
+              className='input w-full'
               type="text"
               name="address"
               required 
@@ -78,9 +78,10 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div>
+        <div className='mb-12 flex items-center gap-5'>
           <input
-            className='h6 w-6 accent-yellow-400'            
+            className='h-6 w-6 accent-yellow-400 focus:outline-none
+             focus:ring focus:ring-yellow-400 focus:ring-offset-2'            
             type="checkbox"
             name="priority"
             id="priority"
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Button({children,disable,to,type}) {
 
- const base =`bg-yellow-400 uppercase font-semibold
+ const base =`bg-yellow-400 text-sm uppercase font-semibold
    text-stone-800  tracking-wide rounded-full inline-block
     transition-all duration-300 hover:bg-yellow-300 focus:outline-none
     focus:ring focus:ring-yellow-300 focus:ring-offset-2
@@ -11,7 +11,7 @@ function Button({children,disable,to,type}) {
     primary:base + ' py-3 px-4 sm:px-6 sm:py-4',
     small:base+ ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
 
-    secondary:`uppercase font-semibold border-2 border-stone-300
+    secondary:`uppercase text-sm font-semibold border-2 border-stone-300
     text-stone-400  tracking-wide rounded-full inline-block
      transition-all duration-300 hover:bg-stone-300 hover:text-stone-800
       focus:outline-none focus:text-stone-800
